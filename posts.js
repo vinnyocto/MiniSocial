463e0991f14bbeda457fddbc605ba79b64d2c682
vinnyocto <vinny.o32904@gmail.com>
2026-05-12 01:03:05 -0700

    Update posts.js

--- posts.js
+++ posts.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { pool } = require('../db');
+const { pool } = require('./db');
 const auth = require('../middleware/auth');
 
 const router = express.Router();
