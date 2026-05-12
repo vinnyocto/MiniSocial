db767f00c4a9f42907fc65c7b4adbb6718460610
vinnyocto <vinny.o32904@gmail.com>
2026-05-12 01:02:20 -0700

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bcrypt  = require('bcryptjs');
 const jwt     = require('jsonwebtoken');
-const { pool } = require('../db');
+const { pool } = require('./db');
 
 const router = express.Router();
 
